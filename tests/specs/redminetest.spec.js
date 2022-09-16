fc8649556dd76b9f23d4f3c6939c58362787e965
Kar1stan <90850632+Kar1stan@users.noreply.github.com>
2022-09-16 20:16:12 +0300

    Update redminetest.spec.js

--- tests/specs/redminetest.spec.js
+++ tests/specs/redminetest.spec.js
@@ -1,37 +1,37 @@
 const { test, expect } = require('@playwright/test');
-const { RedminePageMain } = require('../pageobjects/Redmine.page');
+const { RedmineMainPage } = require('../pageobjects/Redmine.page');
 
 test.describe('Redmine Test', () => {
 
 test('Should sign up with valid credentials', async ({ page }) => {
-  const redminePage = new RedminePageMain(page);
+  const redminePage = new RedmineMainPage(page);
   await redminePage.goto()
   await redminePage.userUpRight();
 });
   
 test('Should sign up with invalid credentials', async ({ page }) => {
-  const redminePage = new RedminePageMain(page);
+  const redminePage = new RedmineMainPage(page);
   await redminePage.goto()
   await redminePage.userUpError();
 });
   
 test('Should sign in with valid credentials', async ({ page }) => {
-  const redminePage = new RedminePageMain(page);
+  const redminePage = new RedmineMainPage(page);
   await redminePage.goto()
   await redminePage.userInRight();
 });
   
 test('Should sign in with invalid credentials', async ({ page }) => {
-  const redminePage = new RedminePageMain(page);
+  const redminePage = new RedmineMainPage(page);
   await redminePage.goto()
   await redminePage.userInError();
 });
   
 test('Should sign in recovery of password', async ({ page }) => {
-  const redminePage = new RedminePageMain(page);
+  const redminePage = new RedmineMainPage(page);
   await redminePage.goto()
   await redminePage.userInPasswordRecovery();
-  await expect(this.page).toHaveURL('https://redmine.org/account/lost_password');
+  await expect(page).toHaveURL('https://redmine.org/account/lost_password');
 });
 
 });
