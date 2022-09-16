6dccac193591587c8dbb38086dccf05bf118efa8
Kar1stan <90850632+Kar1stan@users.noreply.github.com>
2022-09-16 16:18:48 +0300

    Add files via upload

---
+++ tests/specs/redminetest.spec.js
@@ -0,0 +1,32 @@
+const { test, expect } = require('@playwright/test');
+const { RedminePageMain } = require('../pageobjects/RedminePageMain');
+
+test.describe('RedmineTest', () => {
+
+test('redmine test of user up with valid credentials', async ({ page }) => {
+  const redminePage = new RedminePageMain(page);
+  await redminePage.goto()
+  await redminePage.UserUpRight();
+});
+test('redmine test of user up with invalid credentials', async ({ page }) => {
+  const redminePage = new RedminePageMain(page);
+  await redminePage.goto()
+  await redminePage.UserUpError();
+});
+test('redmine test of user in with valid credentials', async ({ page }) => {
+  const redminePage = new RedminePageMain(page);
+  await redminePage.goto()
+  await redminePage.UserInRight();
+});
+test('redmine test of user in with invalid credentials', async ({ page }) => {
+  const redminePage = new RedminePageMain(page);
+  await redminePage.goto()
+  await redminePage.UserInError();
+});
+test('redmine test of user in recovery of password', async ({ page }) => {
+  const redminePage = new RedminePageMain(page);
+  await redminePage.goto()
+  await redminePage.UserInPasswordRecovery() 
+});
+
+});
