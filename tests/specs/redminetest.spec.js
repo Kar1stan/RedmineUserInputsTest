fd5aea41e02a94c0413b1145943c336bc2e6e17d
Kar1stan <90850632+Kar1stan@users.noreply.github.com>
2022-09-16 16:40:40 +0300

    Update redminetest.spec.js

--- tests/specs/redminetest.spec.js
+++ tests/specs/redminetest.spec.js
@@ -8,21 +8,25 @@ test('redmine test of user up with valid credentials', async ({ page }) => {
   await redminePage.goto()
   await redminePage.UserUpRight();
 });
+  
 test('redmine test of user up with invalid credentials', async ({ page }) => {
   const redminePage = new RedminePageMain(page);
   await redminePage.goto()
   await redminePage.UserUpError();
 });
+  
 test('redmine test of user in with valid credentials', async ({ page }) => {
   const redminePage = new RedminePageMain(page);
   await redminePage.goto()
   await redminePage.UserInRight();
 });
+  
 test('redmine test of user in with invalid credentials', async ({ page }) => {
   const redminePage = new RedminePageMain(page);
   await redminePage.goto()
   await redminePage.UserInError();
 });
+  
 test('redmine test of user in recovery of password', async ({ page }) => {
   const redminePage = new RedminePageMain(page);
   await redminePage.goto()
