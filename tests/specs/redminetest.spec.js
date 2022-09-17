c29b46ec59ab90ef5624c9fa19741835c15af707
Kar1stan <90850632+Kar1stan@users.noreply.github.com>
2022-09-17 16:53:51 +0300

    Update redminetest.spec.js

--- tests/specs/redminetest.spec.js
+++ tests/specs/redminetest.spec.js
@@ -7,24 +7,32 @@ test('Should sign up with valid credentials', async ({ page }) => {
   const redminePage = new RedmineMainPage(page);
   await redminePage.goto()
   await redminePage.userUpRight();
+  await expect(page).toHaveURL('https://www.redmine.org/login');
+  await expect(page.locator('#flash_notice')).toBeVisible();
 });
   
 test('Should sign up with invalid credentials', async ({ page }) => {
   const redminePage = new RedmineMainPage(page);
   await redminePage.goto()
   await redminePage.userUpError();
+  await expect(page).toHaveURL('https://redmine.org/account/register');
+  await expect(page.locator('#errorExplanation')).toBeVisible();
 });
   
 test('Should sign in with valid credentials', async ({ page }) => {
   const redminePage = new RedmineMainPage(page);
   await redminePage.goto()
   await redminePage.userInRight();
+  await expect(page).toHaveURL('https://www.redmine.org/login');
+  //was trying to expect selector of the error message but didn't work
 });
   
 test('Should sign in with invalid credentials', async ({ page }) => {
   const redminePage = new RedmineMainPage(page);
   await redminePage.goto()
   await redminePage.userInError();
+  await expect(page).toHaveURL('https://redmine.org/login');
+  await expect(page.locator('#flash_error')).toBeVisible();
 });
   
 test('Should sign in recovery of password', async ({ page }) => {
@@ -32,6 +40,7 @@ test('Should sign in recovery of password', async ({ page }) => {
   await redminePage.goto()
   await redminePage.userInPasswordRecovery();
   await expect(page).toHaveURL('https://www.redmine.org/account/lost_password');
+  //was trying to expect selector of the error message but didn't work
 });
 
 });
