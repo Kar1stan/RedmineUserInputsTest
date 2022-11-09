d59e44fe5530959e1f6a4f03452ad856a6794bd8
Kar1stan <90850632+Kar1stan@users.noreply.github.com>
2022-11-09 16:51:26 +0300

    Update signin.spec.ts

--- tests/specs/signin.spec.ts
+++ tests/specs/signin.spec.ts
@@ -20,7 +20,7 @@ test('Should sign in  with valid credentials', async ({ page }) => {
   await signInPage.signInUsername(username);
   await signInPage.signInPassword(password);
   await signInPage.clickSignInSubmitBtn();
-  await expect(page).toHaveURL('https://redmine.org/login');
+  await expect(page).toHaveURL('https://www.redmine.org/login');
 });
 
 test('Should sign in with invalid credentials', async ({ page }) => {
@@ -29,7 +29,7 @@ test('Should sign in with invalid credentials', async ({ page }) => {
   await signInPage.signInWrongUsername(wrong_username);
   await signInPage.signInWrongPasswordWithOneSimbol(wrong_password);
   await signInPage.clickSignInSubmitBtn();
-  await expect(page).toHaveURL('https://redmine.org/login');
+  await expect(page).toHaveURL('https://www.redmine.org/login');
   await expect(signInPage.signInErrorMessage).toBeVisible();
 });
 
